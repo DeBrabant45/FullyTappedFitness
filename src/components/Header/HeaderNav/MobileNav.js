146a64f63c81022289937cca1d403a95810cf4e6
Aaron <DeBrabant45@users.noreply.github.com>
2019-01-30 15:47:26 -0500

    Update MobileNav.js

--- src/components/Header/HeaderNav/MobileNav.js
+++ src/components/Header/HeaderNav/MobileNav.js
@@ -10,7 +10,7 @@ var isMenuOpen = function(state) {
   
  const MobileNav = () => {
     return (
-            <Menu className="bm-burger-button" className={styles.mobilenav} className={styles.navpage} onStateChange={ isMenuOpen } isOpen={ false }>
+            <Menu onStateChange={ isMenuOpen } isOpen={ false }>
               <ListLink to="/">Home</ListLink>
               <ListLink to="/classes/">Classes</ListLink>
               <ListLink to="/wellness/">Wellness</ListLink>
