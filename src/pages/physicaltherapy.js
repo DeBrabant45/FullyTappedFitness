4893710e133abc8f154940230a0d9da5fd2cbef0
Aaron <DeBrabant45@users.noreply.github.com>
2019-02-03 01:51:39 -0500

    Removing page

--- src/pages/physicaltherapy.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Banner from "../components/Banner/Banner.js";
-
-export default () => (
-  <Layout>
-    <Banner bgimg={{ backgroundColor: 'blue' }} first="DISCOVER" second="LIMITLESS" third="You dont have to be great to start"/>
-  </Layout>
-);
